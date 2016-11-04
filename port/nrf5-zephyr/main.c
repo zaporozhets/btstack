82615f00451ae54f1e8e198757ff64b8230ec091
Matthias Ringwald <matthias@ringwald.ch>
2016-11-04 11:49:33 +0100

    nrf5-zephyr: retarget printf to UART 115200

--- port/nrf5-zephyr/main.c
+++ port/nrf5-zephyr/main.c
@@ -1,9 +1,11 @@
 #include <stdint.h>
+#include <stdio.h>
+
 int btstack_main(void);
 
 /**
  * @brief Function for main application entry.
  */
 int main(void){
-
+	printf("Hello from BTstack\n");
 }
